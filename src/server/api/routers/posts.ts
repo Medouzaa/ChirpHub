2d1f482d7e35622515beda726b81b6c6991556d0
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2023-10-23 10:53:24 +0100

    Create post

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,26 +3,23 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-  // create: publicProcedure
-  //   .input(z.object({ content: z.string().min(1) }))
-  //   .mutation(async ({ ctx, input }) => {
-  //     // simulate a slow db call
-  //     await new Promise((resolve) => setTimeout(resolve, 1000));
+  create: publicProcedure
+    .input(
+      z.object({ content: z.string().min(1), authorId: z.string().min(1) }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      // simulate a slow db call
+      await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  //     return ctx.db.post.create({
-  //       data: {
-  //         content: input.content,
-  //       },
-  //     });
-  //   }),
+      return ctx.db.post.create({
+        data: {
+          authorId: input.authorId,
+          content: input.content,
+        },
+      });
+    }),
 
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.db.post.findMany({ orderBy: { createdAt: "desc" } });
   }),
-
-  // getLatest: publicProcedure.query(({ ctx }) => {
-  //   return ctx.db.post.findFirst({
-  //     orderBy: { createdAt: "desc" },
-  //   });
-  // }),
 });
