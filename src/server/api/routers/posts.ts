d2e8d89c04d7f175887471f2574c06d8ec2d2310
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2023-11-01 17:59:07 +0100

    Profile router

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,5 +1,4 @@
 import { clerkClient } from "@clerk/nextjs";
-import type { User } from "@clerk/nextjs/dist/types/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
@@ -10,14 +9,7 @@ import {
   privateProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    firstName: user.firstName,
-    imageUrl: user.imageUrl,
-  };
-};
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
