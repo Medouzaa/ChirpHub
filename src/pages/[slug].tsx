b17b2d1c1bcc76682fce21ff40b38755c71c7c13
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2023-11-03 20:52:43 +0100

    Profile styling

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,25 +1,8 @@
 import Head from "next/head";
 import { api } from "~/utils/api";
 import Image from "next/image";
-import { Loading, LoadingSpinner } from "~/components/loading";
-
-function ProfileFeed() {
-  const { data: posts, isLoading: isFeedLoading } =
-    api.profile.getPostsByAuthor.useQuery();
-
-  if (isFeedLoading) return <LoadingSpinner />;
-
-  if (!posts) return <div>Something went wrong!</div>;
-  console.log("posts", posts);
-
-  return (
-    <div>
-      {posts.map((post) => (
-        <div key={post.id}>{post.content}</div>
-      ))}
-    </div>
-  );
-}
+import { Loading } from "~/components/loading";
+import { Layout } from "~/components/layout";
 
 export default function ProfilePage() {
   const { data: user, isLoading } = api.profile.getUserByUsername.useQuery({
@@ -32,26 +15,22 @@ export default function ProfilePage() {
   return (
     <>
       <Head>
-        <title>chirp hub</title>
-        <meta name="description" content="chirp hub profile page" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>{user.username}</title>
       </Head>
-      <div className="mx-auto min-h-screen border-x border-slate-400 md:max-w-2xl">
-        <div className="relative h-60 bg-blue-500">
+      <Layout>
+        <div className="relative h-48 bg-slate-600">
           <Image
             src={user.imageUrl}
             alt={`${user.username}'s profile picture`}
             width={128}
             height={128}
-            className="absolute bottom-[-64px] left-4 rounded-full border-4 border-black"
+            className="absolute -bottom-16 left-4 rounded-full border-4 border-black"
           />
         </div>
-        <div className="h-20" />
-        <div className="border-b px-4">
-          <div className="text-2xl font-bold">{user.username}</div>
-        </div>
-        <ProfileFeed />
-      </div>
+        <div className="h-16" />
+        <div className="p-4 text-2xl font-bold">{user.username}</div>
+        <div className="w-full border-b border-slate-400" />
+      </Layout>
     </>
   );
 }
