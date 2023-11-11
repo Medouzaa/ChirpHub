bfa4485fa2db97b81fdbd646d4fa5e1370be0f97
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2023-11-11 15:59:23 +0100

    Get the username dynamicly

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,12 +1,34 @@
 import Head from "next/head";
-import { api } from "~/utils/api";
+import { type RouterOutputs, api } from "~/utils/api";
 import Image from "next/image";
-import { Loading } from "~/components/loading";
+import { Loading, LoadingSpinner } from "~/components/loading";
 import { Layout } from "~/components/layout";
+import { PostView } from "~/components/post-view";
+import { useRouter } from "next/router";
+
+type FilterUser = RouterOutputs["profile"]["getUserByUsername"];
+function ProfileFeed(props: { user: FilterUser }) {
+  const { data: posts, isLoading: isFeedLoading } =
+    api.profile.getPostsByAuthor.useQuery();
+
+  if (isFeedLoading) return <LoadingSpinner />;
+
+  if (!posts) return <div>Something went wrong!</div>;
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <PostView post={post} author={props.user} key={post.id} />
+      ))}
+    </div>
+  );
+}
 
 export default function ProfilePage() {
+  const router = useRouter();
+  const username = router.query.slug as string;
   const { data: user, isLoading } = api.profile.getUserByUsername.useQuery({
-    username: "ou-zaa",
+    username,
   });
   if (isLoading) return <Loading />;
 
@@ -30,6 +52,7 @@ export default function ProfilePage() {
         <div className="h-16" />
         <div className="p-4 text-2xl font-bold">{user.username}</div>
         <div className="w-full border-b border-slate-400" />
+        <ProfileFeed user={user} />
       </Layout>
     </>
   );
